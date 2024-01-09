40f73c262c7c1241934e9d346bb6a51d7c4daf6f
Ste79ITA <nastri.s@gmail.com>
2024-01-09 18:30:26 +0100

    ADDED models relations

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'content', 'slug'];
+
+    public function type()
+    {
+
+        return $this->belongsTo(Type::class);
+    }
 }
