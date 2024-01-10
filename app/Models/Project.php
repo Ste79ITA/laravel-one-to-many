3108b87f17fc4436a5fc8b43cdeb13919e7de061
Ste79ITA <nastri.s@gmail.com>
2024-01-10 15:48:37 +0100

    ADDED table relations to models Technology and Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -13,7 +13,11 @@ class Project extends Model
 
     public function type()
     {
-
         return $this->belongsTo(Type::class);
+    }
+
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
     }
 }
